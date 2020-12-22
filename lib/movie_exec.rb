66460eb5ab70c1e3b6bef16b5e9981891a65ff66
iamShah <formal>
2020-12-21 20:19:08 -0500

    Done.

--- lib/movie_exec.rb
+++ lib/movie_exec.rb
@@ -2,11 +2,6 @@ require "pry"
 require "rest-client"
 require "json"
 
-
-# require "./lib/SW_Movie/movie_CLI"
-# require "./lib/SW_Movie/movie_API"
-# require "./lib/SW_Movie/actor"
-
 require_relative "./SW_Movie/movie_CLI"
 require_relative "./SW_Movie/movie_API"
 require_relative "./SW_Movie/actor"
